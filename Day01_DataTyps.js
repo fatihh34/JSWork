2a19a2ffe16d55a0dd0ebd245ffedf41f25d7345
Fatih I <117350581+fatihh34@users.noreply.github.com>
2023-07-05 18:04:24 +0300

    v3

---
+++ Day01_DataTyps.js
@@ -0,0 +1,25 @@
+console.clear();
+
+var name = 'JavaScript'
+console.log(typeof(name));
+
+var name = "java"
+console.log(typeof(name))
+
+var myNumber = 1235
+console.log(typeof(myNumber))
+
+var myNumber = 12.5
+console.log(typeof(myNumber))
+
+var isTrue = true
+console.log(isTrue)
+console.log(typeof(isTrue))
+
+var pickedColour = null
+console.log(pickedColour)
+console.log(typeof(pickedColour))
+
+var tanimsiz;
+console.log(tanimsiz)
+console.log(typeof(tanimsiz))
